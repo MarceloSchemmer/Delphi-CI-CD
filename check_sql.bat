1961aac06bc49c0c5b56e3266773f90378999c9f
Schemmer Marcelo <34945753+MarceloSchemmer@users.noreply.github.com>
2024-05-20 12:13:04 -0300

    ajustes

--- check_sql.bat
+++ check_sql.bat
@@ -2,7 +2,6 @@
 setlocal enabledelayedexpansion
 set "diretorio=C:\Users\marce\OneDrive\Documentos\GitHub\Delphi-CI-CD"
 for %%F in ("%diretorio%\*.pas") do (
-    rem Use o comando FINDSTR para verificar se o arquivo contém a string
     findstr /C:"SELECT * FROM" "%%F" >nul
     if !errorlevel! equ 0 (
         echo O arquivo %%~nxF contem a string "SELECT * FROM".
